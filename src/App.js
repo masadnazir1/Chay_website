78282e2c21969d3349b933837c978b9ea18aa497
noetic-asad <asad@noeticworld.com>
2024-03-24 16:40:19 +0500

    Tracking Api added --asad

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
@@ -8,33 +8,44 @@ import AboutPage from './components/AboutPage';
 import Modal from './components/Modal'; // Import Modal component
 import Breadcrumb from './components/Breadcrumb';
 
-
-
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await fetch('http://staging.ssf.org.pk/index.php');
+                console.log('API call successful');
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+        fetchData();
+    }, []); // Empty dependency array to run the effect only once when the component mounts
 
-  return (
-    <Router>
-      <div className="App">
-        <Header openModal={openModal} /> {/* Pass openModal function to Header */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/tea" element={<TeaPage />} />
-          <Route path="/coffee" element={<CoffeePage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-        <Modal isOpen={isModalOpen} onClose={closeModal} /> {/* Render Modal component */}
-      </div>
-    </Router>
-  );
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return (
+        <Router>
+            <div className="App">
+                <Header openModal={openModal} /> {/* Pass openModal function to Header */}
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/tea" element={<TeaPage />} />
+                    <Route path="/coffee" element={<CoffeePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                </Routes>
+                <Modal isOpen={isModalOpen} onClose={closeModal} /> {/* Render Modal component */}
+            </div>
+        </Router>
+    );
 }
 
 export default App;
